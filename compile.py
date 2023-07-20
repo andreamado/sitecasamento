7f8b6803dc1c9d7500769a8c2478dbf62e4f011f
André Amado <andre.amado@pm.me>
2023-07-20 18:12:52 +0100

    created contact page; updated other info page

--- compile.py
+++ compile.py
@@ -1,9 +1,89 @@
-from os import system
-from glob import glob
+from os import system, remove
 
+navbaritems = [
+    {
+        'pt': ['Home', 'index'],
+        'en': ['Home', 'index_en'],
+        'disabled': False
+    },{
+        'pt': ['A nossa história', 'historia'],
+        'en': ['Our story', 'story'],
+        'disabled': True
+    },{
+        'pt': ['Como chegar', 'comochegar'],
+        'en': ['How do I get there?', 'howdoigetthere'],
+        'disabled': False
+    },{
+        'pt': ['Outras informações', 'outrasinformacoes'],
+        'en': ['Other info', 'otherinfo'],
+        'disabled': False
+    },{
+        'pt': ['Fotos (a seguir ao casamento)', 'fotos'],
+        'en': ['Photos (after the wedding)', 'photos'],
+        'disabled': True
+    },{
+        'pt': ['Contacto', 'contacto'],
+        'en': ['Talk to us', 'contact'],
+        'disabled': False
+    }
+]
+
+def generate_json(item, language):
+    with open(f'{folder}data.json', 'w', encoding="utf-8") as f:
+        f.write('{\n')
+
+        # navbar
+        f.write('    "navbar-items": [\n')
+        for i, navitem in enumerate(navbaritems):
+            active = 'true' if navitem == item else 'false'
+            disabled = 'true' if navitem['disabled'] else 'false'
+            link = '#' if navitem == item else navitem[language][1] + '.html'
+
+            f.write( '            {\n')
+            f.write(f'                "name": "{navitem[language][0]}",\n')
+            f.write(f'                "active": {active},\n')
+            f.write(f'                "disabled": {disabled},\n')
+            f.write(f'                "link": "{link}"\n')
+            if i != len(navbaritems) - 1:
+                f.write( '            },\n')
+            else:
+                f.write( '            }\n')
+        f.write('    ],\n')
+        # end navbar
+
+        # language menu
+        f.write( '    "languages": [\n')
+
+        active = 'true' if language == 'pt' else 'false'
+        link = '#' if language == 'pt' else item['pt'][1] + '.html'
+        f.write( '        {\n')
+        f.write( '            "name": "Português",\n')
+        f.write(f'            "active": {active},\n')
+        f.write(f'            "link": "{link}"\n')
+        f.write( '        },\n')
+
+        active = 'true' if language == 'en' else 'false'
+        link = '#' if language == 'en' else item['en'][1] + '.html'
+        f.write( '        {\n')
+        f.write( '            "name": "English",\n')
+        f.write(f'            "active": {active},\n')
+        f.write(f'            "link": "{link}"\n')
+        f.write( '        }\n')
+
+        f.write('    ]\n')
+        # end language menu
+
+        f.write('}')
+
+# folder containing the mustache templates
 folder = 'mustache/'
 
-for file in glob(f"{folder}*.html"):
-    basename = file[len(folder):-5]
-    system(f'mustache -p {folder}navbar.mustache {folder}data/{basename}.json {file} > {basename}.html')
-    print(f'created {basename}.html')
+for item in navbaritems:
+    for language in ['pt', 'en']:
+        if not item['disabled']:
+            generate_json(item, language)
+
+            basename = item[language][1]
+            system(f'mustache -p {folder}navbar.mustache {folder}data.json {folder}{basename}.html > {basename}.html')
+
+remove(f'{folder}data.json')
